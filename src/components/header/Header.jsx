8906acd13d998bda6a945903aa177c5fb0e78220
devthigas <thi.deusefiel@gmail.com>
2022-05-25 18:38:24 -0300

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container header_container">
+                <h5>Oi, Eu sou</h5>
+                <h1>Thigas</h1>
+                <h5 className="text-light">oi</h5>
+            </div>
+        </header>
+    )
+}
+
+export default Header
+
+
